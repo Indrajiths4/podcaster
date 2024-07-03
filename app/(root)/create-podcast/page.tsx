9c048c53d3f41693fe7e3f50c620d1eccf87bb38
Indrajiths4 <indrajithmundackal@gmail.com>
2024-07-03 19:56:20 +0530

    initial commit

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePodcast = () => {
+  return (
+    <div>
+      <h1 className='text-20 text-white-1 font-bold'>Create Podcasts</h1>
+    </div>
+  )
+}
+
+export default CreatePodcast
---
+++ app/(root)/discover/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Discover = () => {
+  return (
+    <div>
+      <h1 className='text-20 text-white-1 font-bold'>Discover</h1>
+    </div>
+  )
+}
+
+export default Discover
