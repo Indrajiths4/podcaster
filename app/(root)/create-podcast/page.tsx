169d030ec8a847edd0bc43c9794809822ad93a01
Indrajiths4 <indrajithmundackal@gmail.com>
2024-07-18 23:30:24 +0530

    paramaters generate podcast

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -133,7 +133,15 @@ const createPodcast = () => {
         </div>
 
         <div className="flex flex-col pt-10">
-          <GeneratePodcast />
+          <GeneratePodcast 
+            setAudioStorageId={setAudioStorageId}
+            setAudio={setAudioUrl}
+            voiceType={voiceType}
+            audio={audioUrl}
+            voicePrompt={voicePrompt}
+            setVoicePrompt={setVoicePrompt}
+            setAudioDuration={setAudioDuration}
+          />
           <GenerateThumbnail />
 
           <div className="mt-10 w-full">
